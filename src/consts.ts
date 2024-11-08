4a9c913883b24e83c3f9ccd950bffb6767f9f8e6
reversed-coffee <nick@reversed.coffee>
2024-11-07 20:49:19 -0600

    Add consts.ts

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "nick's brew";
+
+export const CLEAR_LINK = "https://reversed.coffee";
+export const ONION_LINK = "http://brewz5li5caoioneubizdvuatc25solgnytis27uecxvz45tnnayocqd.onion";
