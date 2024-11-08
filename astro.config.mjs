2829ccd8d693104730374695487a391d207330f3
reversed-coffee <nick@reversed.coffee>
2024-11-07 20:49:01 -0600

    Refactor Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,24 @@
 // @ts-check
+import { CLEAR_LINK } from './src/consts';
+
 import { defineConfig } from 'astro/config';
+
+// Remark plugins for blog, injection, new tab.
 import { remarkReadTime } from './remark/readTime.mjs';
 import { remarkInjectAds } from './remark/injectAds.mjs';
 import { remarkLinksNewTab } from './remark/linksNewTab.mjs';
 
+// Astro integrations - MDX, icon embedding, sitemap.
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
+// Configuration for Astro.
 export default defineConfig({
-	site: 'https://reversed.coffee',
+	site: CLEAR_LINK,
 	integrations: [mdx(), sitemap(), icon({ iconDir: './static/icons' })],
     markdown: {
-        // @ts-ignore
+        // @ts-ignore - I don't care about type checking since it's going to JS.
         remarkPlugins: [remarkReadTime, remarkInjectAds, remarkLinksNewTab],
     },
     publicDir: './static'
